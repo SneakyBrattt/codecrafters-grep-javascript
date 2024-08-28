8b1a8e0e18799b9f6146e2a56b7b3dcab86b35da
brat <144054981+SneakyBrattt@users.noreply.github.com>
2024-08-28 17:34:35 +0530

    run tests

--- app/main.js
+++ app/main.js
@@ -1,5 +1,8 @@
 function matchPattern(inputLine, pattern) {
   let regex = pattern.replace('\\d', '[0-9]').replace('\\w', '[0-9a-zA-Z]');
+  if(pattern[0]==='^'){
+    pattern=pattern.slice(1,pattern.length);
+  }
   if (pattern.length === 1) {
     return inputLine.includes(pattern);
   }
