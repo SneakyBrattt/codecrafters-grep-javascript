b0caeea7534f10aa48252ca414a75115b458beeb
brat <144054981+SneakyBrattt@users.noreply.github.com>
2024-08-28 17:30:19 +0530

    pass stage

--- app/main copy.js
+++ app/main copy.js
@@ -2,8 +2,22 @@ function matchPattern(inputLine, pattern) {
   let regex = pattern.replace('\\d', '[0-9]').replace('\\w', '[0-9a-zA-Z]');
   if (pattern.length === 1) {
     return inputLine.includes(pattern);
+  }else if(pattern=== "\\d"){
+    return /\d/.test(inputLine)
+  }else if (pattern === "\\w"){
+    return /\w/.test(inputLine)
+  } else if (pattern.startsWith("[") && pattern.endsWith("]")) {
+    let groupPattern = pattern.substring(1, pattern.length - 1)
+    let regex = new RegExp("[" + groupPattern + "]");
+    return regex.test(inputLine);
+  }else if (pattern.startsWith("[^") && pattern.endsWith("]")) {
+
+    let groupPattern = pattern.substring(2, pattern.length - 1);
+    let regex = new RegExp("[^" + groupPattern + "]");
+    return regex.test(inputLine);
   }
   else {
+    throw new Error(`Unhandled pattern ${pattern}`);
     return new RegExp(regex).test(inputLine);
   }
 }
