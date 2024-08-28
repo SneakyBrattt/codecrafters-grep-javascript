a28773f07d00821d25e99500573bd9371eb57552
brat <144054981+SneakyBrattt@users.noreply.github.com>
2024-08-28 15:58:25 +0530

    pass stage

--- your_program.sh
+++ your_program.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#
+
 # Use this script to run your program LOCALLY.
 #
 # Note: Changing this script WILL NOT affect how CodeCrafters runs your program.
@@ -12,4 +12,4 @@ set -e # Exit early if any commands fail
 #
 # - Edit this to change how your program runs locally
 # - Edit .codecrafters/run.sh to change how your program runs remotely
-exec node app/main.js "$@"
+exec node  app/main.js "$@"
