347a3f497c619d1bc856d90b5237eb6f269f8014
brat <144054981+SneakyBrattt@users.noreply.github.com>
2024-08-28 18:04:55 +0530

    run tests

--- app/main.js
+++ app/main.js
@@ -1,10 +1,11 @@
 function matchPattern(inputLine, pattern) {
   let regex = pattern.replace('\\d', '[0-9]').replace('\\w', '[0-9a-zA-Z]');
+  pattern = pattern.replace(/\\(\d+)/g, (_, groupNumber) => `\\${groupNumber}`);
   if (pattern.length === 1) {
     return inputLine.includes(pattern);
-  } else if (pattern.includes('+') || pattern.includes('?') || pattern.includes('|') || pattern.includes('.') || (pattern.startsWith('[') && pattern.endsWith(']')) || pattern.startsWith('\\d') || pattern.startsWith('^') || pattern.endsWith('$')) {
+  } else if (pattern.includes('+') || pattern.includes('?') || pattern.includes('|') || pattern.includes('.') || (pattern.startsWith('[') && pattern.endsWith(']')) || pattern.startsWith('\\d') || pattern.startsWith('^') || pattern.endsWith('$')|| pattern.includes('\\')) {
     const regExp = new RegExp(pattern);
-    console.log({result: regExp.test(inputLine)})
+    console.log({ result: regExp.test(inputLine) })
     return regExp.test(inputLine);
   }
   else {
@@ -25,11 +26,11 @@ function main() {
   console.log("Logs from your program will appear here");
 
   //Uncomment this block to pass the first stage
-   if (matchPattern(inputLine, pattern)) {
-     process.exit(0);
-   } else {
-     process.exit(1);
-   }
+  if (matchPattern(inputLine, pattern)) {
+    process.exit(0);
+  } else {
+    process.exit(1);
+  }
 }
 
 main();
